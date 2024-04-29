7249d5488b40598aeddac56c3e63b376ffd3347c
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-29 15:51:11 +0500

    word counter

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        message: "Enter your sentence: ",
+        type: "input"
+    }
+]);
+const words = answer.sentence.split(" ");
+console.log("This is your sentence", words);
+console.log(`Your sentence word count is ${words.length}`);
